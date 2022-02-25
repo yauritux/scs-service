fb16bc16ca1736e695e7dbfe44edfd2385a9dfbb
Yauri Attamimi <yauritux@gmail.com>
2022-02-25 13:33:45 +0700

    finalize HeaderCommandControllerWebClientTest

--- scs-writer/src/test/java/id/go/beacukai/scswriter/infrastructure/adapter/web/controller/HeaderCommandControllerWebClientTest.java
+++ scs-writer/src/test/java/id/go/beacukai/scswriter/infrastructure/adapter/web/controller/HeaderCommandControllerWebClientTest.java
@@ -30,7 +30,7 @@ public class HeaderCommandControllerWebClientTest {
     static String HEADER_URL = "/v2/headers";
 
     @Test
-    void createNewHeader() {
+    void createNewHeader_headerIsCreated() {
         var newDocumentHeader = new Header();
         newDocumentHeader.setIdHeader(UUID.randomUUID().toString());
         newDocumentHeader.setAsalData("W");
@@ -64,7 +64,7 @@ public class HeaderCommandControllerWebClientTest {
     }
 
     @Test
-    void createNewHeader_badRequest() {
+    void createNewHeader_badRequest_shouldGetAnErrorMessage() {
         var newDocumentHeader = new Header();
         newDocumentHeader.setIdHeader(UUID.randomUUID().toString());
 
@@ -82,5 +82,58 @@ public class HeaderCommandControllerWebClientTest {
                     assertNotNull(responseBody);
                     assertEquals(expectedErrorMessage, responseBody);
                 });
+    }
+
+    @Test
+    void updateHeader_existingHeader_recordIsUpdated() {
+        var idHeader = UUID.randomUUID().toString();
+
+        var updatedHeader = new Header();
+        updatedHeader.setIdHeader(idHeader);
+        updatedHeader.setAsalData("W");
+        updatedHeader.setIdPerusahaan("1234567890");
+        updatedHeader.setNamaPerusahaan("PT. DEMO PORTAL");
+        updatedHeader.setRoleEntitas("IMPORTIR");
+        updatedHeader.setKodeDokumen("20");
+
+        when(headerCommandServiceMock.updateDocumentHeader(updatedHeader, idHeader)).thenReturn(Mono.just(updatedHeader));
+
+        webTestClient
+                .put()
+                .uri(HEADER_URL + "/{id}", idHeader)
+                .bodyValue(updatedHeader)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(Header.class)
+                .consumeWith(headerEntityExchangeResult -> {
+                    var responseBody = headerEntityExchangeResult.getResponseBody();
+                    assertNotNull(responseBody);
+                    assertEquals(updatedHeader.getIdHeader(), responseBody.getIdHeader());
+                    assertEquals(updatedHeader.getKodeDokumen(), responseBody.getKodeDokumen());
+                });
+    }
+
+    @Test
+    void updateHeader_nonExistingHeader_gotNotFoundResponse() {
+        var idHeader = UUID.randomUUID().toString();
+
+        var updatedHeader = new Header();
+        updatedHeader.setIdHeader(idHeader);
+        updatedHeader.setAsalData("W");
+        updatedHeader.setIdPerusahaan("1234567890");
+        updatedHeader.setNamaPerusahaan("PT. DEMO PORTAL");
+        updatedHeader.setRoleEntitas("IMPORTIR");
+        updatedHeader.setKodeDokumen("20");
+
+        when(headerCommandServiceMock.updateDocumentHeader(updatedHeader, idHeader)).thenReturn(Mono.empty());
+
+        webTestClient
+                .put()
+                .uri(HEADER_URL + "/{id}", idHeader)
+                .bodyValue(updatedHeader)
+                .exchange()
+                .expectStatus()
+                .isNotFound();
     }
 }
